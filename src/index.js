5287992b07d65af15cb062b27e6267ce72dfb5a3
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2019-03-19 17:17:14 +0800

    1.初步实现对取消响应的选择

--- src/index.js
+++ src/index.js
@@ -2,73 +2,111 @@ const globalInput = document.createElement("input")
 globalInput.type = "file"
 globalInput.style.opacity = "0"
 
+//文件类型过滤器
+class FileTypeFilter{
+    constructor(acceptsString){
+        let accepts = acceptsString.split(",").map(type => type.trim()).filter(type => type)
+        this.accepts = accepts
+
+        let acceptTests = accepts.map(type => {
+            if (/^\./.test(type)){ //为后缀
+                return {
+                    target: "name", //检查名称
+                    regExp: new RegExp(`${type.replace(".", "\\.")}$`, "i")
+                }
+            } else { //为MIME类型
+                if (/\/\*$/){ //泛匹配
+                    return {
+                        target: "type", //检查名称
+                        regExp: new RegExp(`^${type.replace("*", "\\S+")}$`, "i")
+                    }
+                } else {
+                    return {
+                        target: "type", //检查名称
+                        regExp: new RegExp(`^${type}$`, "i")
+                    }
+                }
+            }
+        })
+
+        this.acceptTests = acceptTests
+    }
+
+    filter(files){
+        if (this.acceptTests.length === 0) return files
+
+        return files.filter(file => {
+            return this.acceptTests.some(test => {
+                return test.regExp.test(file[test.target])
+            })
+        })
+    }
+
+    getInputAccept(){
+        return this.accepts.join(",")
+    }
+}
+
 //选取文件函数
-function selectFile(accept = "", size){
+function selectFile(accept = "", size, ){
     //后缀格式为 .xxx类型
     //MIME为  xxxx/yy 或者 xxxx/*
     //逗号分隔
     let input = globalInput
-    let accepts = accept.split(",").map(type => type.trim()).filter(type => type)
-    input.accept = accepts.join(",")
-
-    let acceptTests = accepts.map(type => {
-        if (/^\./.test(type)){ //为后缀
-            return {
-                target: "name", //检查名称
-                regExp: new RegExp(`${type.replace(".", "\\.")}$`, "i")
-            }
-        } else { //为MIME类型
-            if (/\/\*$/){ //泛匹配
-                return {
-                    target: "type", //检查名称
-                    regExp: new RegExp(`^${type.replace("*", "\\S+")}$`, "i")
-                }
-            } else {
-                return {
-                    target: "type", //检查名称
-                    regExp: new RegExp(`^${type}$`, "i")
-                }
-            }
-        }
-    })
-
+    let typeFilter = new FileTypeFilter(accept)
+    input.accept = typeFilter.getInputAccept()
+    input.multiple = true
+    
     return new Promise((resolve, reject) => {
         input.onchange = e => {
-            let file = input.files[0]
-            if (!file) return
+            let files = input.files
+            if (files.length === 0) resolve([])
 
-            if (size && file.size > size){
-                reject(new TypeError("ERROR_FILE_SIZE"))
-                return
-            }
+            files = typeFilter.filter(Array.from(files))
+            resolve(files)
 
-            let result = true
-            if (acceptTests.length > 0) {
-                result = acceptTests.some(test => {
-                    return test.regExp.test(file[test.target])
-                })
-            }
+            document.removeEventListener("wheel", cancle, true)
+            document.removeEventListener("mousemove", cancle, true)
+            document.removeEventListener("keydown", cancle, true)
 
-            if (result){
-                resolve(file)
-            } else {
-                reject(new TypeError("ERROR_FILE_TYPE"))
-            }
+            // if (size && file.size > size){
+            //     reject(new TypeError("ERROR_FILE_SIZE"))
+            //     return
+            // }
+
+            // let result = true
+            // if (acceptTests.length > 0) {
+            //     result = acceptTests.some(test => {
+            //         return test.regExp.test(file[test.target])
+            //     })
+            // }
+
+            // if (result){
+            //     resolve(file)
+            // } else {
+            //     reject(new TypeError("ERROR_FILE_TYPE"))
+            // }
 
             input.value = ""
         }
+
+        function cancle(){
+            document.removeEventListener("wheel", cancle, true)
+            document.removeEventListener("mousemove", cancle, true)
+            document.removeEventListener("keydown", cancle, true)
+            reject("cancle")
+        }
+
+        //通过这三个来获取用户的取消动作
+        document.addEventListener("wheel", cancle, true)
+        document.addEventListener("mousemove", cancle, true)
+        document.addEventListener("keydown", cancle, true)
         
         //兼容IE Input不在DOM树上无法触发选择的问题
         document.body.appendChild(input)
         input.click()
-        document.body.removeChild(input)        
+        document.body.removeChild(input)
     })
 }
-
-//通过这三个来获取用户的取消动作
-document.addEventListener("wheel", _ => console.log("wheel"), true)
-document.addEventListener("mouseup", _ => console.log("mouseup"))
-document.addEventListener("keydown", _ => console.log("keydown"), true)
-
 
 module.exports = selectFile
